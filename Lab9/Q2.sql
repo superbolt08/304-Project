fd93adf5e6e2d1c6bfaf969b3850dc4d79fc7ff7
bbmbabrb <mr.brandonmar@gmail.com>
2024-11-28 19:47:21 -0800

    fixed spelling

--- Lab9/Q2.sql
+++ Lab9/Q2.sql
@@ -1,6 +1,6 @@
 -- Q2.1
 CREATE VIEW deptSummary AS
-SELECT dept.dno, dept.dname, Count(*) as totalEmp, sum(salary) as totalSalary
+SELECT dept.dno, dept.dname, COUNT(*) as totalEmp, SUM(salary) as totalSalary
 FROM dept JOIN emp ON dept.dno = emp.dno
 GROUP BY dept.dno
 
